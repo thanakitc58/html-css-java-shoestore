4089c2f9137d61c03521e88df94cef3e13153778
unknown <vx7vx8vx9@gmail.com>
2024-10-18 16:16:22 +0700

    first commit

---
+++ scrip.js
@@ -0,0 +1,23 @@
+// function ShowMessage(){
+//     let msg = document.getElementById("mytext").value;
+//     // alert(msg);
+//     let headtext = document.getElementById("headtext");
+//     headtext.innerHTML = msg;
+// }
+
+// -----------
+// let todos =["wake up", "eat", "sleep"];
+// console.log(todos.length);
+// for(let i = 0; i < todos.length; i++){
+//     console.log(todos[i]);
+// }
+//  for (let i in todos){
+//     console.log(todos[i]);
+// }
+// -----------
+
+// ประกาศตัวแปร Object
+let user = {
+     name : "karn",
+     age : "35",
+}
